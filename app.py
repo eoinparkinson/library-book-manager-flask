ddb975148217e0630b4b0e5f1a6f7141a7536661
unknown <EoinParkinson2015@DOMAIN.local>
2020-09-08 10:31:27 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import base64
+import requests
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+#Basic image url to base64 (working)
+@app.route("/")
+def my_form():
+    return render_template("my-form.html")
+
+@app.route("/", methods=["POST"])
+def my_form_post():
+    text = request.form["book-name"]
+    processed_text = text.lower()
+    return processed_text
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
