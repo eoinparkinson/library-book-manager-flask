6b3c913d01c2f99630b2e7c2a1ee2be8f576d698
unknown <EoinParkinson2015@DOMAIN.local>
2020-09-09 09:48:59 +0100

    added updates

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-import base64
+# import sqlite3
 import requests
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-#Basic image url to base64 (working)
+"""
+DB_PATH = './books_database.db'   # Update this path accordingly
+NOTSTARTED = 'Not Started'
+INPROGRESS = 'In Progress'
+COMPLETED = 'Completed'
+"""
+
+"""
+#Calling home directory
 @app.route("/")
 def my_form():
     return render_template("my-form.html")
+"""
 
-@app.route("/", methods=["POST"])
+@app.route("/")
 def my_form_post():
-    text = request.form["book-name"]
-    processed_text = text.lower()
-    return render_template("my-form.html", message=processed_text)
+    if request.method == "POST":
+        if request.form["submitButton"] == "searchBook":
+            text = request.form["book-name"]
+            processed_text = text.lower()
+            return render_template("my-form.html", message=processed_text)
+
+        elif request.form["submitButton"] == "addBook":
+            return render_template("add-book.html")
+
+        else:
+            return render_template("my-form.html", message="")
+
+    else:
+        return render_template("my-form.html", message="")
 
 
 if __name__ == "__main__":
