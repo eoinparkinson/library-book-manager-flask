6621f9f40de6de41e4c546a5ba99d466097bb8d4
eoinparkinson <eoinparkinson@users.noreply.github.com>
2020-09-11 23:15:03 +0100

    updates again

--- app.py
+++ app.py
@@ -4,35 +4,22 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-"""
-DB_PATH = './books_database.db'   # Update this path accordingly
-NOTSTARTED = 'Not Started'
-INPROGRESS = 'In Progress'
-COMPLETED = 'Completed'
-"""
 
-"""
-#Calling home directory
-@app.route("/")
-def my_form():
-    return render_template("my-form.html")
-"""
 # Homepage
 @app.route("/", methods=["GET","POST"])
 def searchBookForm():
     if request.method == "POST":
-        if request.form["submitButton"] == "searchBook":
+        if request.form["submitButton"] == "Search":
             text = request.form["book-name"]
             processed_text = text.lower()
-            return render_template("my-form.html", message=processed_text)
-
-        elif request.form["submitButton"] == "addBook":
-            return render_template("add-book.html")
+            return render_template("my-form.html", message="Returning search results for: "+processed_text)
 
         else:
+            text = ""
             return render_template("my-form.html", message="")
 
     else:
+        text = ""
         return render_template("my-form.html", message="")
 
 # Add Book Page
